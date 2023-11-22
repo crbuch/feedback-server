8ee480c373f627bb03fb360dbde8020b981cc028
Cooper Buch <cbuch690@gmail.com>
2023-11-22 14:39:25 -0600

    changed run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Start the Python server in the background
-gnome-terminal --title="Python Server" -- bash -c "python server.py"
-
-# Start ngrok in a new terminal window
-gnome-terminal --title="Ngrok Server" -- bash -c "ngrok http http://localhost:5000"
+python server.py & ngrok http http://localhost:5000
